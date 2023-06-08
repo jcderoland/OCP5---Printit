8ca97cfcf0f71b9317422d3b7aad2c8e912a7ce4
JC De Roland <jc.deroland@gmail.com>
2023-06-08 02:17:29 +0200

    feat(script.js)event listener added on arrows

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,88 @@
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: "./assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "./assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "./assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
 
-let arrow_left = document.getElementById("arrow_left");
+// Ajout Event Listener sur Arrows
+
+const arrow_left = document.getElementById("arrow_left");
 arrow_left.addEventListener("click", () => {
   console.log("J'ai cliqué sur le bouton gauche");
 });
 
-let arrow_right = document.getElementById("arrow_right");
+const arrow_right = document.getElementById("arrow_right");
 arrow_right.addEventListener("click", () => {
   console.log("J'ai cliqué sur le bouton droit");
 });
 
-let dotSpan = `
-<span class="dot"></span>
-`;
+// Ajout 1 bullet points par slide (4 au total)
 
-let dots = document.querySelector(".dots");
+const dots = document.querySelector(".dots");
 
-for (i = 0; i < slides.length; i++) {
-  dots.innerHTML += dotSpan;
+for (let i = 0; i < slides.length; i++) {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+  dot.classList.add(`dot-${i}`);
+  dots.appendChild(dot);
 }
 
-let dot_selected = document.querySelector(".dot");
+// Différencier point de slide en cours de visionnage
+// (C'est la 1ère)
+
+let dot_selected = document.querySelector(".dot-0");
 dot_selected.classList.add("dot_selected");
+
+// Modifiez image au clic sur flèche droite
+
+const bannerImg = document.querySelector(".banner-img");
+const bannerTag = document.querySelector(".banner-tag");
+let currentSlide = 0;
+
+arrow_right.addEventListener("click", () => {
+  if (currentSlide === slides.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  bannerImg.setAttribute("src", slides[currentSlide].image);
+  bannerTag.innerHTML = slides[currentSlide].tagLine;
+
+  dot_selected = document.querySelector(".dot_selected");
+  dot_selected.classList.remove("dot_selected");
+
+  dot_selected = document.querySelector(`.dot-${currentSlide}`);
+  dot_selected.classList.add("dot_selected");
+});
+
+// Modifiez image au clic sur flèche gauche
+
+arrow_left.addEventListener("click", () => {
+  if (currentSlide === 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide--;
+  }
+  bannerImg.setAttribute("src", slides[currentSlide].image);
+  bannerTag.innerHTML = slides[currentSlide].tagLine;
+
+  dot_selected = document.querySelector(".dot_selected");
+  dot_selected.classList.remove("dot_selected");
+
+  dot_selected = document.querySelector(`.dot-${currentSlide}`);
+  dot_selected.classList.add("dot_selected");
+});
